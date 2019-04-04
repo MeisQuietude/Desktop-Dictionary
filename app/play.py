caa2a1297f4dcd563fcba4ad79d4c74d3c762f61
Quietude <savelyev_stepan@mail.ru>
2019-04-04 23:38:47 +0500

    github hello

--- app/play.py
+++ app/play.py
@@ -1,21 +1,32 @@
 import random
 from cfg.color import Color
+
 clr = Color()
 
 
-def play_en(words: dict):
-    print("I ask you only english words now.")
-    print(clr.cyan("Start to play :>"))
-    pass
+def play_kv(words: dict):
+    print(clr.cyan("Choose game mode now:"))
+    print("~ 1: only first word in pair")
+    print("~ 2: only second word in pair")
+    print("~ 3: all words in pair")
+    print(clr.cyan("~ 3 is default."))
+    try:
+        ans = int(input("Game mod: "))
+        assert 0 < ans < 4
+    except ValueError:
+        ans = 3
+    except AssertionError:
+        ans = 3
+    print(clr.cyan(f"Start to play game mode {ans} :>"))
+    play(words, ans)
 
 
-def play_ru(words: dict):
-    print("I ask you only russian words now.")
-    print(clr.cyan("Start to play :>"))
-    pass
+def play(words: dict, rule: int):
+    indent = '\t'
+    ask = [x for x in words]
+    print(random.choice([x for x in words]))
 
 
-def play_mix(words: dict):
-    print("I ask you only both english and russian words now.")
-    print(clr.cyan("Start to play :>"))
-    pass
+if __name__ == '__main__':
+    d = {'apple': 'яблоко', 'orange': 'апельсин'}
+    play_kv(d)
